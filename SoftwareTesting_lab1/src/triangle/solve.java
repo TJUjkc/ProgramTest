5de66c1aa85e07666b719f9136e60799c301c2fb
jkc <jkc@163.com>
2019-03-19 21:02:17 +0800

    lab1 重交

--- SoftwareTesting_lab1/src/triangle/solve.java
+++ SoftwareTesting_lab1/src/triangle/solve.java
@@ -3,15 +3,17 @@ package triangle;
 import java.util.Scanner;
 
 public class solve {
-	int numofcoins[]= {1,1,2,3};
-	int coins[]= {50,20,5,1};
-	public boolean result(int x) {
-		for(int i=0;i<4;i++) {
-			for(int j=0;j<numofcoins[i];j++) {
-				if(x>=coins[i]) {
-					x-=coins[i];
+	int numofcoins[] = {1, 1, 2, 3};//硬币数量
+	int coins[] = {50, 20, 5, 1};//硬币面额
+	
+	//如果可以被取出，返回true，否则返回false.
+	public boolean FindResult(int x) {
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < numofcoins[i]; j++) {
+				if(x >= coins[i]) {
+					x -= coins[i];
 				}
-				if(x==0) return true;
+				if(x == 0) return true;
 			}
 		}
 		return false;
@@ -19,10 +21,10 @@ public class solve {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); 
-		int x = sc.nextInt();;
+		int x = sc.nextInt();
 		solve s = new solve();
-		boolean ans = s.result(x);
-		if(ans==true)
+		boolean ans = s.FindResult(x);
+		if(ans == true)
 			System.out.println("yes");
 		else
 			System.out.println("no");
--- SoftwareTesting_lab1/test_src/triangle/test_solve.java
+++ SoftwareTesting_lab1/test_src/triangle/test_solve.java
@@ -1,16 +1,35 @@
 package triangle;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.*;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith (Parameterized.class)
 public class test_solve {
+	public int x;
+	public boolean result;
+	
+	public test_solve(int x, boolean result){
+		this.x = x;
+		this.result = result;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> calcValues(){
+		return Arrays.asList(new Object [][] {{26, true}, {19, false}});
+	}
+	
 	@Test
 	public void test_result(){
-		solve s=new solve();
-		assertEquals(true,s.result(26));
-		assertEquals(true,s.result(27));
-		assertEquals(false,s.result(-1));
-		assertEquals(false,s.result(100));
-		assertEquals(false,s.result(19));
+		solve s = new solve();
+		assertEquals(result, s.FindResult(x));
 	}
+	
 }
